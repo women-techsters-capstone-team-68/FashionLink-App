97f7a0004dbb4eef34113af744089153b6fcbb08
Amarachi-Promise <chimamandapromise21@gmail.com>
2026-02-21 21:27:18 +0100

    move backend project to backend-dev branch

--- create-databases.sql
+++
@@ -1,7 +0,0 @@
--- Create databases for Fashion Link application
-CREATE DATABASE IF NOT EXISTS database_development;
-CREATE DATABASE IF NOT EXISTS database_test;
-CREATE DATABASE IF NOT EXISTS database_production;
-
--- Display created databases
-SHOW DATABASES;
--- models/artisanprofile.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require('sequelize');
-
-module.exports = (sequelize) => {
-  return sequelize.define('ArtisanProfile', {
-    specialties: DataTypes.JSON,
-    location: DataTypes.STRING,
-    rating: DataTypes.FLOAT
-  });
-};
